64bf744e32c494ee66e13d273d5d172ca3b4a1f0
Yash Dewangan <yashpd13@gmail.com>
2024-04-15 07:40:56 +0530

    update dev hub

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -5,6 +5,7 @@ import React, { ReactNode } from "react";
 import { useIsClient } from "usehooks-ts";
 import { ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton } from "./recommended";
+import { FollowingSkeleton } from "./following";
 
 interface WrapperProps {
   children: ReactNode;
@@ -18,6 +19,7 @@ const Wrapper = ({ children }: WrapperProps) => {
     return (
       <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
         <ToggleSkeleton />
+        <FollowingSkeleton />
         <RecommendedSkeleton />
       </aside>
     );
--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -1,6 +1,25 @@
 import { getSelf } from "./auth-service";
 import { db } from "@/lib/db";
 
+export const getFollowedUsers = async () => {
+  try {
+    const self = await getSelf();
+
+    const followedUser = await db.follow.findMany({
+      where: {
+        followerId: self.id,
+      },
+      include: {
+        following: true,
+      },
+    });
+
+    return followedUser;
+  } catch (error) {
+    return [];
+  }
+};
+
 export const isFollowingUser = async (id: string) => {
   try {
     const self = await getSelf();
