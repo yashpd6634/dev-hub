f43994d6eb9881092e3628a70b11ecf30ca0e13f
Yash Dewangan <yashpd13@gmail.com>
2024-04-21 20:43:42 +0530

    added chat layout

---
+++ components/stream-player/variant-toggle.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ChatVariant, useChatSidebar } from "@/store/use-chat-sidebar";
+import { MessagesSquare, Users } from "lucide-react";
+import React from "react";
+import Hint from "@/components/hint";
+import { Button } from "@/components/ui/button";
+
+const ChatToggle = () => {
+  const { variant, onChangeVariant } = useChatSidebar((state) => state);
+
+  const isChat = variant === ChatVariant.CHAT;
+
+  const Icon = isChat ? Users : MessagesSquare;
+
+  const onToggle = () => {
+    const newVariant = isChat ? ChatVariant.COMMUNITY : ChatVariant.CHAT;
+    onChangeVariant(newVariant);
+  };
+
+  const label = isChat ? "Community" : "Go back to chat";
+
+  return (
+    <Hint label={label} side="left" asChild>
+      <Button
+        onClick={onToggle}
+        variant="ghost"
+        className="h-auto p-2 hover:bg-white/10 hover:text-primary bg-transparent"
+      >
+        <Icon className="h-4 w-4" />
+      </Button>
+    </Hint>
+  );
+};
+
+export default ChatToggle;
