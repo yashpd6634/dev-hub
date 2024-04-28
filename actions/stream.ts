ecb96954a25575d17cadf3e38321cf729b2c24d7
Yash Dewangan <yashpd13@gmail.com>
2024-04-28 15:32:29 +0530

    added info card

--- actions/stream.ts
+++ actions/stream.ts
@@ -16,6 +16,7 @@ export const updateStream = async (values: Partial<Stream>) => {
 
     const validData = {
       name: values.name,
+      thumbnailUrl: values.thumbnailUrl,
       isChatEnabled: values.isChatEnabled,
       isChatDelayed: values.isChatDelayed,
       isChatFollowersOnly: values.isChatFollowersOnly,
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -8,6 +8,8 @@ import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { cn } from "@/lib/utils";
 import Chat, { ChatSkeleton } from "@/components/stream-player/chat";
 import ChatToggle from "@/components/stream-player/chat-toggle";
+import Header, { HeaderSkeleton } from "./header";
+import InfoCard from "./info-card";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -45,6 +47,20 @@ export const StreamPlayer = ({
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
+          <InfoCard
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            name={stream.name}
+            thumbnailUrl={stream.thumbnailUrl}
+          />
         </div>
         <div className={cn("col-span-1", collapsed && "hidden")}>
           <Chat
@@ -67,6 +83,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-1 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
