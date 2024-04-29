3275c9152fe1d475fafbe8a5bbf0a49b4d0d4e4c
Yash Dewangan <yashpd13@gmail.com>
2024-04-29 09:15:20 +0530

    fixed security issue

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks(.*)", "/api/uploadthing", "/:username"],
+  publicRoutes: ["/", "/api/webhooks(.*)", "/api/uploadthing", "/:username", "/search"],
 });
 
 export const config = {
