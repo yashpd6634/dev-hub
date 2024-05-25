8b97ecc7670908820b9152c852f035b29c3d061b
Yash Dewangan <yashpd13@gmail.com>
2024-05-25 10:21:30 +0530

    fix: fixed livekit

--- actions/ingress.ts
+++ actions/ingress.ts
@@ -21,12 +21,18 @@ const roomService = new RoomServiceClient(
   process.env.LIVEKIT_API_SECRET!
 );
 
-const ingressClient = new IngressClient(process.env.LIVEKIT_API_URL!);
+const ingressClient = new IngressClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY,
+  process.env.LIVEKIT_API_SECRET
+);
 
 export const resetIngresses = async (hostIdentity: string) => {
   const ingresses = await ingressClient.listIngress({
     roomName: hostIdentity,
   });
+
+  console.log(ingresses, "start ingresses");
 
   const rooms = await roomService.listRooms([hostIdentity]);
 
@@ -39,17 +45,24 @@ export const resetIngresses = async (hostIdentity: string) => {
       await ingressClient.deleteIngress(ingress.ingressId);
     }
   }
+
+  console.log(ingresses, "end engresses");
 };
 
 export const createIngress = async (ingressType: IngressInput) => {
   const self = await getSelf();
+  console.log(self, "self");
   const selfStream = await getStreamByUserId(self.id);
+
+  console.log(selfStream, "selfStream");
 
   if (!selfStream) {
     throw new Error("Stream not found");
   }
 
   await resetIngresses(self.id);
+
+  console.log("reset Ingresses done!");
 
   const options: CreateIngressOptions = {
     name: self.username,
@@ -77,7 +90,12 @@ export const createIngress = async (ingressType: IngressInput) => {
     });
   }
 
+  console.log(options, "options");
+  console.log(ingressType, "ingressType");
+  console.log(ingressClient, "ingressClient");
+
   const ingress = await ingressClient.createIngress(ingressType, options);
+  console.log(ingress, "ingress");
 
   if (!ingress || !ingress.url || !ingress.streamKey) {
     throw new Error("Failed to create ingress");
